3d2a4eeab2095a7e3c7d25b60b8db18752f552a8
constuelo <ferguson.rory@gmail.com>
2019-05-23 11:45:34 +0100

    update

--- anagrams.py
+++ anagrams.py
@@ -9,23 +9,47 @@
 
 import unittest
 import itertools
+import time
+
+"""
+- Optimise the code for fast retrieval
+    - Look at algorithms
+
+- Optimise results (anagrams.get_anagrams('petals'))
+     - sorted() = ~0.06-0.07 secs
+     - Itertools permutation = ~0.45-0.5 secs
+
+- thread safe operations
+     - sorted() - yes
+     - list comprehension - yes
+"""
 
 class Anagrams:
 
     def __init__(self):
-        self.words = open('words.txt').readlines()
+        self.words = open('words.txt', 'r').readlines()
+        self.words_list = self.words_to_list()
+
+    def words_to_list(self):
+        return [word.strip().lower() for word in self.words]
 
     def get_anagrams(self, word):
-        pass
-
-class TestAnagrams(unittest.TestCase):
-
-    def test_anagrams(self):
-        anagrams = Anagrams()
-        self.assertEquals(anagrams.get_anagrams('plates'), ['palest', 'pastel', 'petals', 'plates', 'staple'])
-        self.assertEquals(anagrams.get_anagrams('eat'), ['ate', 'eat', 'tea'])
+        return [a for a in self.words_list if sorted(word) == sorted(a)]
 
 
 if __name__ == '__main__':
-    unittest.main()
+    start = time.time()
+    anagrams = Anagrams()
+    print(anagrams.get_anagrams('petals'))  # ['palest', 'pastel', 'petals', 'plates', 'staple']
+    print(f'\nValidation took {float("%0.2f" % (time.time() - start))} seconds to run')
 
+
+
+
+#     def get_anagrams(self, word):
+#         """ list comprehension / itertools permutation """
+#         return [w for w in anagram(word) if w in self.words_list]
+
+# def anagram(word):
+#     for w in itertools.permutations(word):
+#         yield ''.join(w)
---
+++ anagrams_copy.py
@@ -0,0 +1,131 @@
+# pylint: disable=R0903, C0111
+from operator import itemgetter
+from collections import Counter, defaultdict
+import unittest
+import logging
+
+
+class WordNotFoundError(KeyError):
+    """
+    Error raised when word not in anagram list
+    """
+
+
+class Anagrams:
+    """
+    Class provides anagrams of a given word.
+    Keyword arguments:
+    strict_mode (boolean) -- enable error raising mode (default False)
+    Limitations - if a valid word exists, but is not in the list,
+    it will not be recognised as potential anagram.
+    This is for performance reasons, as well as unspecified
+    behaviour concerning unknown words.
+    This only uses threadsafe objects, so will be threadsafe.
+    """
+    def __init__(self, strict_mode=False):
+        self.strict_mode = strict_mode
+
+        # Load in words, and strip whitespace characters
+        with open('words.txt') as words_file:
+            words = [word.strip().lower() for word in words_file.readlines()]
+
+        # Produce the maps of word to counters, and counters to words
+        input_word_map = {}
+        reverse_map = defaultdict(list)
+        counters = []
+        for word in words:
+            counter_list = list(Counter(word).items())
+            counter_list.sort(key=itemgetter(0))
+            counter_tuple = tuple(counter_list)
+            counters.append(counter_tuple)
+            input_word_map[word] = counter_tuple
+            reverse_map[counter_tuple].append(word)
+
+        # Connect up words to their anagrams
+        self.anagram_map = {}
+        for word, counter in input_word_map.items():
+            anagram_list = reverse_map[counter]
+            anagram_list.sort()
+            self.anagram_map[word] = anagram_list
+
+    def get_anagrams(self, word):
+        """
+        Get the anagrams of a given word.
+        Keyword arguments
+        word - word you are trying to find anagrams for (case insensitive)
+        If strict mode is off (default), then if a word isn't found
+        it'll return a list with that word in, and raise a logging warning.
+        If strict mode is on, then if a word isn't found a WordNotFoundError
+        is raise.
+        """
+        lower_word = word.lower()
+        if lower_word in self.anagram_map:
+            return self.anagram_map[lower_word]
+        if self.strict_mode:
+            raise WordNotFoundError("{} not found".format(lower_word))
+        else:
+            logging.warning(
+                "%s was not found in the list of words", lower_word)
+            return [lower_word]
+
+
+class TestAnagrams(unittest.TestCase):
+    """
+    Test Anagrams class functionality"
+    """
+
+    def test_lower_case_anagrams(self):
+        """
+        Ensure that default functionality, that the anagram is returned.
+        """
+        anagrams = Anagrams()
+        self.assertEqual(anagrams.get_anagrams('plates'), [
+            'palest', 'pastel', 'petals', 'plates', 'staple'])
+        self.assertEqual(
+            anagrams.get_anagrams('eat'), ['ate', 'eat', 'tea'])
+
+    def test_mixed_case_anagrams(self):
+        """
+        Check that mixed case words still work.
+        """
+        anagrams = Anagrams()
+        self.assertEqual(anagrams.get_anagrams('peTAls'), [
+            'palest', 'pastel', 'petals', 'plates', 'staple'])
+        self.assertEqual(
+            anagrams.get_anagrams('EaT'), ['ate', 'eat', 'tea'])
+
+    def test_input_is_in_output(self):
+        """
+        Check that the input word is found in the output list
+        """
+        anagrams = Anagrams()
+        self.assertIn('petals', anagrams.get_anagrams('petals'))
+        self.assertIn('abide', anagrams.get_anagrams('abide'))
+
+    def test_strict_mode_input_not_in_word_list(self):
+        """
+        Test that in strict mode, a word not in the list
+        raises a WordNotFoundError
+        """
+        anagrams = Anagrams(strict_mode=True)
+        with self.assertRaises(WordNotFoundError):
+            anagrams.get_anagrams("cheeseburger")
+
+    def test_default_mode_input_not_in_word_list(self):
+        """
+        Test that in default (non-strict mode) a warning is raised
+        and the word is returned as a single item in a list.
+        """
+        anagrams = Anagrams()
+        with self.assertLogs(level="WARNING") as logging_capture:
+            self.assertEqual(
+                anagrams.get_anagrams('cheeseburger'),
+                ['cheeseburger']
+            )
+        self.assertEqual(
+            logging_capture.output, [
+                'WARNING:root:cheeseburger was not found in the list of words']
+            )
+
+if __name__ == '__main__':
+    unittest.main()
