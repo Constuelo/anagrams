f691f00294338f2a64f82541802e29be0cd50e53
constuelo <ferguson.rory@gmail.com>
2019-05-24 08:38:34 +0100

    Clean up

--- permutation.py
+++ permutation.py
@@ -1,13 +1,4 @@
-# Given a words.txt file containing a newline-delimited list of dictionary
-# words, please implement the Anagrams class so that the get_anagrams() method
-# returns all anagrams from words.txt for a given word.
-#
-# Requirements:
-#   - Optimise the code for fast retrieval
-#   - Thread Safe implementation
-#   - Write more tests
 
-import unittest
 
 class Anagrams:
 
