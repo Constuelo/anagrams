2f55547aad4bab9e1fd6884b40c6c5660a9c8d5e
constuelo <ferguson.rory@gmail.com>
2019-06-10 13:26:11 +0100

    Removed files

--- anagrams.py
+++ anagrams.py
@@ -9,40 +9,17 @@
 
 import unittest
 
-"""
-This code is written in python 3.7.2
-
-- DeprecationWarnings ignored
-- ResourceWarnings ignored
-- with open() to auto close file after read
-
-- Optimise results (anagrams.get_anagrams('petals'))
-    - sorted() = ~0.06-0.08 secs
-    - Itertools permutation = ~0.45-0.5 secs
-
-- thread safe operations
-    - sorted() - yes
-    - list comprehension - yes
-    - open() - yes
-"""
 
 class Anagrams:
-
+    
     def __init__(self):
         with open('words.txt') as reader:
             self.words = reader.readlines()
         self.words_list = [word.strip('\n') for word in self.words]
 
     def get_anagrams(self, word):
-        """ Returns all anagrams from self.words_list for a given word.
-            :param: word
-            :ptype: str
-            :rtype: list
-        """
-        if isinstance(word, str):
-            return [a for a in self.words_list if sorted(word.lower()) == sorted(a.lower())]
-        else:
-            print(f'{word} is not a str')
+        return [a for a in self.words_list if sorted(word.lower()) == sorted(a.lower())]
+
 
 class TestAnagrams(unittest.TestCase):
 
